8280dd6705b634fae043d4a6f8af1607a435ba0d
omar_thompson <omarshakkaithompson@gmail.com>
2024-08-08 02:24:10 +1000

    Completed bug fixes with SplashScreen, started MainFrame, started MainPanel

--- src/ui/panel/MainPanel.java
+++ src/ui/panel/MainPanel.java
@@ -2,7 +2,56 @@
 // will contain the options to navigate to the following panels.
 // Play, High Scores, Configuration, Exit
 
+// Working on placements and button sizes before i link them into other panels. - Omar
+
 package ui.panel;
 
-public class MainPanel {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class MainPanel extends JPanel {
+
+    public MainPanel() {
+        //Placing a border around the Panel so i can see where everything fits.
+        setBorder(BorderFactory.createLineBorder(Color.GREEN, 1));
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.NORTH;
+
+        // Placed a border around the title and to see where it is.
+        JLabel titleLabel = new JLabel("Tetris");
+        titleLabel.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 36));
+        titleLabel.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
+        add(titleLabel, gbc);
+
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        // Placed a border around the button panel to see where the panel is.
+        JPanel buttons = new JPanel(new GridBagLayout());
+        buttons.setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
+        GridBagConstraints buttonConstraints = new GridBagConstraints();
+        buttonConstraints.fill = GridBagConstraints.HORIZONTAL;
+
+        //this allows me to put space around the buttons apparently.
+        buttonConstraints.insets = new Insets(5, 5, 5, 5);
+
+        buttons.add(new JButton("Play"), buttonConstraints);
+        buttons.add(new JButton("Configure"), buttonConstraints);
+        buttons.add(new JButton("High Scores"), buttonConstraints);
+        buttons.add(new JButton("Exit"), buttonConstraints);
+
+        gbc.weighty = 1;
+        add(buttons, gbc);
+    }
 }
+
+
+
+
+
+
+
